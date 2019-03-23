e6c32ad1f314d6d534b323d432a6a9062163b6fe
William Tsang <william.tsang@slalom.com>
2019-03-22 23:49:04 -0400

    initial form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
     return (
+    <div>
+      <div>
+        <div className="bg-primary float-right">Div 1</div>
+        <div className="bg-warning float-right">Div 2</div>
+        <div className="float-right">Div 3</div>
+      </div>
+      <div class="mt-5 clearfix">
+        <label className="bg-primary">Label 1</label>
+        <label className="bg-warning">Label 2</label>
+      </div>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p class='mt-5'>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>kalamo hehe app</p>
+          <form>
+            <div>
+              What are you looking for?:
+            </div>
+            <div className = "mt-4 mb-4 ">
+              <input type = "text" name = "name"/>
+            </div>
+            <div className="text-left">
+              <input className = "btn btn-primary" type = "submit" value = "submit"/>
+            </div>  
+            </form>
         </header>
       </div>
+    </div>
     );
   }
 }
